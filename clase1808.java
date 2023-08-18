b2b99bde58cfac7c9c8709112c49df8310414042
Pablo Cardozo <cardozopabloj@gmail.com>
2023-08-18 19:45:22 -0300

    .

---
+++ clase1808.java
@@ -0,0 +1,70 @@
+// public class clase1808 {
+//     public static void main(String[] args) {
+//         // int dato;
+//         // dato = 10;
+//         // System.out.println(dato);
+
+//         char letra = 'A';
+//         String nombre = "Pablo";
+//         boolean esVerdad = true;
+//         int num1 = 20;
+//         float num2 = 10.5f;
+//         byte i = 42;
+//         short depto = 1000;
+//         long superficie = 1000000;
+
+//         System.out.println("La letra: " + letra);
+//         System.out.println("Tu nombre: " + nombre);
+//         System.out.println("Es verdad? " + esVerdad);
+//         System.out.println("Numero: " + num1);
+//         System.out.println("Numero: " + num2);
+//         System.out.println("Numero: " + i);
+//         System.out.println("Numero: " + depto);
+//         System.out.println("Numero: " + superficie);
+//     }
+// }
+
+// // ##################################################################################
+
+// public class clase1808 {
+//     public static void main(String[] args) {
+//         char[] palabra = {'a','b','c'};
+//         for (char letra: palabra) {
+//             System.out.println(letra + " ");
+//         } 
+//         System.out.println();
+
+//         int[] numeros = {10, 20, 30, 40, 50};
+//         for (int num : numeros) {
+//             System.out.println(num + " ");
+//         }
+//         System.out.println();
+
+//         boolean[] verdaderoFalso = {true, false, false, true};
+//         for (boolean rta : verdaderoFalso) {
+//             System.out.println(rta + " ");
+//         }
+//         System.out.println();
+
+//         String[] nombres = {"Pablo", "Santi"};
+//         for (String nombre : nombres) {
+//             System.out.println(nombre + " ");
+//         }
+//     }
+// }
+
+// ##################################################################################
+
+public class clase1808 {
+    public static void main(String[] args) {
+        // for (int x = 5; x <= 40; x+=5) {
+        //     System.out.println(x);
+        // }
+
+        int x = 5;
+        while (x <= 50) {
+            System.out.println(x);
+            x+=5;
+        }
+    }
+}
