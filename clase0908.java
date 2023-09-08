d6df4754d0d8e07ccd9cf5be08d49ca9dd0c8f1f
Pablo Cardozo <cardozopabloj@gmail.com>
2023-09-08 16:41:35 -0300

    0908

--- clase0908.java
+++ clase0908.java
@@ -1,3 +1,44 @@
+// import java.util.Scanner;
+// import java.util.Random;
+
+// public class clase0908{
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         Random random = new Random();
+    
+//         int randomNum = random.nextInt(100);
+//         System.out.println(randomNum);
+
+//         int num = 0;
+//         int intentos = 0;
+
+//         do {
+
+//             boolean esNum = false;
+//             do {
+//                 try {
+//                     System.out.print("Ingresa tu intento: "); 
+//                     num = scanner.nextInt();
+//                     esNum = true;
+//                 } catch (java.util.InputMismatchException e1) {
+//                     System.out.println("Solo puedes ingresar numeros enteros"); 
+//                     scanner.nextLine();
+//                 }
+//             } while(!esNum);
+            
+//             if (num > randomNum) {
+//                 System.out.println("Menor"); 
+//                 intentos++;
+//             } else if (num < randomNum) {
+//                 System.out.println("Mayor"); 
+//                 intentos++;
+//             } else {
+//                 System.out.println("Congratz. El numero era " + randomNum + ", adivinarlo te tomo " + intentos + " intentos"); 
+//             }
+//         } while (num != randomNum);
+//     }
+// }
+
 import java.util.Scanner;
 import java.util.Random;
 
@@ -5,36 +46,39 @@ public class clase0908{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-    
-        int randomNum = random.nextInt(100);
-        System.out.println(randomNum);
 
-        int num = 0;
-        int intentos = 0;
+        boolean incluirMayusculas = true;
+        boolean incluirMinusculas = true;
+        boolean incluirDigitos = true;
 
-        do {
+        System.out.print("Ingresa la longitud: ");
+        int longitud = scanner.nextInt();
+        int count = 0;
 
-            boolean esNum = false;
-            do {
-                try {
-                    System.out.print("Ingresa tu intento: "); 
-                    num = scanner.nextInt();
-                    esNum = true;
-                } catch (java.util.InputMismatchException e1) {
-                    System.out.println("Solo puedes ingresar numeros enteros"); 
-                    scanner.nextLine();
+        while (count <= longitud) {
+            int sigRandom = random.nextInt(3);
+
+            if (sigRandom == 0) {
+                if (incluirMayusculas) {
+                    String mayusculas = "ABCDEFGHIJKLMNOPQRSTUVW";
+                    int indiceRandom = random.nextInt(mayusculas.length());
+                    System.out.print(mayusculas.charAt(indiceRandom));
                 }
-            } while(!esNum);
-            
-            if (num > randomNum) {
-                System.out.println("Menor"); 
-                intentos++;
-            } else if (num < randomNum) {
-                System.out.println("Mayor"); 
-                intentos++;
-            } else {
-                System.out.println("Congratz. El numero era " + randomNum + ", adivinarlo te tomo " + intentos + " intentos"); 
+            } else if (sigRandom == 1) {
+                if (incluirMinusculas) {
+                    String minusculas = "abcdefghijklmnopqrstuvwxyz";
+                    int indiceRandom = random.nextInt(minusculas.length());
+                    System.out.print(minusculas.charAt(indiceRandom));
+                }
+            } else if (sigRandom == 2) {
+                if (incluirDigitos) {
+                    String digitos = "123456789";
+                    int indiceRandom = random.nextInt(digitos.length());
+                    System.out.print(digitos.charAt(indiceRandom));
+                }
             }
-        } while (num != randomNum);
+
+            count++;
+        }
     }
 }
