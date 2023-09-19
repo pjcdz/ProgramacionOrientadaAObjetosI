208ef9b8841108a4fe256e8bde5936ced409cbc6
Pablo Cardozo <cardozopabloj@gmail.com>
2023-09-19 20:26:55 -0300

    1509

--- clase0915.java
+++ clase0915.java
@@ -48,90 +48,110 @@
 
 // ########################################################################################################################
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class clase0915 {
-    public static void main(String args[]) {
-        // CuentaBancaria cuentaBancaria = new CuentaBancaria("Pablo Cardozo", 100);
+// public class clase0915 {
+//     public static void main(String args[]) {
+//         // CuentaBancaria cuentaBancaria = new CuentaBancaria("Pablo Cardozo", 100);
 
-        // System.out.println("Info: ");
-        // System.out.println(cuentaBancaria.getTitular());
-        // System.out.println(cuentaBancaria.getSaldo());
+//         // System.out.println("Info: ");
+//         // System.out.println(cuentaBancaria.getTitular());
+//         // System.out.println(cuentaBancaria.getSaldo());
 
-        // System.out.println("Ingreso de 300");
-        // cuentaBancaria.depositar(300);
+//         // System.out.println("Ingreso de 300");
+//         // cuentaBancaria.depositar(300);
 
-        // System.out.println("Info: ");
-        // System.out.println(cuentaBancaria.getTitular());
-        // System.out.println(cuentaBancaria.getSaldo());
+//         // System.out.println("Info: ");
+//         // System.out.println(cuentaBancaria.getTitular());
+//         // System.out.println(cuentaBancaria.getSaldo());
 
-        // System.out.println("Retiro de 100");
-        // cuentaBancaria.retirar(100);
+//         // System.out.println("Retiro de 100");
+//         // cuentaBancaria.retirar(100);
 
-        // System.out.println("Info: ");
-        // System.out.println(cuentaBancaria.getTitular());
-        // System.out.println(cuentaBancaria.getSaldo());
+//         // System.out.println("Info: ");
+//         // System.out.println(cuentaBancaria.getTitular());
+//         // System.out.println(cuentaBancaria.getSaldo());
 
-        CuentaBancaria cuenta1 = new CuentaBancaria("Pablo", 100);
-        CuentaBancaria cuenta2 = new CuentaBancaria("Sol", 1000);
-        Banco banco = new Banco();
+//         CuentaBancaria cuenta1 = new CuentaBancaria("Pablo", 100);
+//         CuentaBancaria cuenta2 = new CuentaBancaria("Sol", 1000);
+//         Banco banco = new Banco();
 
-        System.out.println("--------  Info: --------  ");   
-        System.out.println(cuenta1.getSaldo());
-        System.out.println(cuenta2.getSaldo());
+//         System.out.println("--------  Info: --------  ");   
+//         System.out.println(cuenta1.getSaldo());
+//         System.out.println(cuenta2.getSaldo());
 
-        // cuenta2.realizarTransferencia(100, cuenta1); // forma 1
-        banco.realizarTransferencia(100, cuenta2, cuenta1); // forma 2
+//         // cuenta2.realizarTransferencia(100, cuenta1); // forma 1
+//         banco.realizarTransferencia(100, cuenta2, cuenta1); // forma 2
 
-        System.out.println("--------  Info: --------  ");   
-        System.out.println(cuenta1.getSaldo());
-        System.out.println(cuenta2.getSaldo());
+//         System.out.println("--------  Info: --------  ");   
+//         System.out.println(cuenta1.getSaldo());
+//         System.out.println(cuenta2.getSaldo());
 
-    }
-}
+//     }
+// }
 
-class Banco {
-    void realizarTransferencia(double cantidad, CuentaBancaria cuentaOrigen, CuentaBancaria cuentaDestino) {
-        cuentaOrigen.retirar(cantidad);
-        cuentaDestino.depositar(cantidad);
-    }
-}
+// class Banco {
+//     void realizarTransferencia(double cantidad, CuentaBancaria cuentaOrigen, CuentaBancaria cuentaDestino) {
+//         cuentaOrigen.retirar(cantidad);
+//         cuentaDestino.depositar(cantidad);
+//     }
+// }
 
-class CuentaBancaria {
-    String titular;
-    double saldo;
+// class CuentaBancaria {
+//     String titular;
+//     double saldo;https://open.spotify.com/artist/2Waw2sSbqvAwK8NwACNjVo
 
-    CuentaBancaria(String titular, double saldo) {
-        this.titular = titular;
-        this.saldo = saldo;
-    }
+//     CuentaBancaria(String titular, double saldo) {
+//         this.titular = titular;
+//         this.saldo = saldo;
+//     }
 
-    String getTitular() {
-        return titular;
-    }
+//     String getTitular() {
+//         return titular;
+//     }
 
-    double getSaldo() {
-        return saldo;
-    }
+//     double getSaldo() {
+//         return saldo;
+//     }
 
-    void depositar(double cantidad) {
-        saldo += cantidad;
-    }
+//     void depositar(double cantidad) {
+//         saldo += cantidad;
+//     }
 
-    void retirar(double cantidad) {
-        saldo -= cantidad;
-    }
+//     void retirar(double cantidad) {
+//         saldo -= cantidad;
+//     }
 
-    // void depositar(double cantidad, CuentaBancaria cuenta) {
-    //     cuenta.saldo += cantidad;
-    // }
+//     // void depositar(double cantidad, CuentaBancaria cuenta) {
+//     //     cuenta.saldo += cantidad;
+//     // }
 
-    // void retirar(double cantidad, CuentaBancaria cuenta) {
-    //     cuenta.saldo -= cantidad;
-    // }
+//     // void retirar(double cantidad, CuentaBancaria cuenta) {
+//     //     cuenta.saldo -= cantidad;
+//     // }
 
-    // void realizarTransferencia(double cantidad, CuentaBancaria cuentaDestino) {
-    //     retirar(cantidad, this);
-    //     depositar(cantidad, cuentaDestino);
-    // }
-}
+//     // void realizarTransferencia(double cantidad, CuentaBancaria cuentaDestino) {
+//     //     retirar(cantidad, this);
+//     //     depositar(cantidad, cuentaDestino);
+//     // }
+// }
+
+// ########################################################################################################################
+
+// public class clase0915 {
+//     public static void main(String args[]) {
+
+//     }
+// }
+
+// class vehiculo {
+//     String marca;
+//     String modelo;
+
+//     vehiculo(String marca, String modelo) {
+//         this.marca = marca;
+//         this.modelo = modelo;
+//     }
+
+//     void acelerar() { }
+// }
