719df1b84b36b5dbe7f47395e6cdd4ae2f1f13db
Pablo Cardozo <cardozopabloj@gmail.com>
2023-10-23 21:03:01 -0300

    listo

--- c1020/GameOfLifeV2/Partida.java
+++ c1020/GameOfLifeV2/Partida.java
@@ -7,14 +7,17 @@ public class Partida {
     Partida(int tamanio) {
         this.tamanio = tamanio;
         this.mapa = new Celula[tamanio][tamanio];
+        // Tablero tablero = new Tablero();
+        // mapa = tablero.generarMapa(tamanio);
     }
 
 
-    void play() {
+    void play() { // Todo lo de esta funcion se podria poner directamente en el constructor
+        
         Tablero tablero = new Tablero();
         mapa = tablero.generarMapa(tamanio);
         
-        System.out.println( mapa[2][2].getVecinos(mapa) );
+        // System.out.println( mapa[2][2].getVecinos(mapa) );
     }
 
     
@@ -34,17 +37,30 @@ public class Partida {
         }
     }
 
-    void evolucionar() {
+    int[][] getVecinosVivos(int tamanio) {
+        int[][] vecinosVivos = new int[tamanio][tamanio];
+
         for(int x = 0; x < tamanio; x++) {
             for(int y = 0; y < tamanio; y++) {
-                int vecinosVivos = mapa[x][y].getVecinos(mapa);
-                // System.out.println(vecinosVivos);
+                vecinosVivos[x][y] = mapa[x][y].getVecinos(mapa);
+            }
+        }
+
+        return vecinosVivos;
+    }
+
+    void evolucionar() {
+        int[][] vecinosVivos = getVecinosVivos(tamanio);
+
+        for(int x = 0; x < tamanio; x++) {
+            for(int y = 0; y < tamanio; y++) {
+                // System.out.println(vecinosVivos[x][y]);
                 if ( mapa[x][y].getViva() ) {
-                    if ( vecinosVivos < 2 || vecinosVivos > 3 ) {
+                    if ( vecinosVivos[x][y] < 2 || vecinosVivos[x][y] > 3 ) {
                         mapa[x][y].setViva(false);
                     } 
                 } else {
-                    if ( vecinosVivos == 3 ) {
+                    if ( vecinosVivos[x][y] == 3 ) {
                         mapa[x][y].setViva(true);
                     }
                 }
