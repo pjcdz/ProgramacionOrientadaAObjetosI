60f0c0391dacb37c1881de111d98bd64debfe8ed
Pablo Cardozo <85005529+pjcdz@users.noreply.github.com>
2024-03-07 14:57:23 -0300

    sdf

--- c1214/Final/Ej02/App.java
+++ c1214/Final/Ej02/App.java
@@ -3,10 +3,14 @@ package c1214.Final.Ej02;
 public class App {
     public static void main(String args[]) {
         // System.out.println("asd");
-        Juego juego = new Juego("Pepe", "Bosque encantado");
-        juego.iniciar();
+        // Juego juego = new Juego("Pepe", "Bosque encantado");
+        // juego.iniciar();
         // juego.escenas.get(escena.getDescripcion()).agregarObjeto(objeto);
         // juego.jugador.tomarObjeto(objeto);
         // juego.jugador.usarObjeto(objeto);
+
+        for (int a=0, b=10; a<b; a++, b++) {
+            System.out.println(a + " + " + b);
+        }
     }
 }
